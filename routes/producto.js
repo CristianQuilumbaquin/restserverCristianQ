b79c42e3f67e32380e33153f06a50aa02b8140d2
CristianQuilumbaquin <73316535+CristianQuilumbaquin@users.noreply.github.com>
2021-05-17 12:28:23 -0500

    Add files via upload

---
+++ routes/producto.js
@@ -0,0 +1,27 @@
+const { Router } = require('express');
+const {
+    ProductoGet,
+    ProductoPost,
+    ProductoPut,
+    ProductoDelete,
+
+} = require('../controllers/producto');
+
+const { check } = require('express-validator');
+const { validarJWT } = require('../middlewares/validar-jwt');
+
+
+
+const router = Router();
+
+router.get('/', validarJWT, ProductoGet);
+
+router.post('/', validarJWT, ProductoPost);
+
+router.put('/:id', validarJWT, ProductoPut);
+
+router.delete('/:id', validarJWT, ProductoDelete);
+
+
+
+module.exports = router;
